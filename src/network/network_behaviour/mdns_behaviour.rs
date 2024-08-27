800c148154280b6b627117933f4c1d1b89b67de0
dne33 <dne33@uclive.ac.nz>
2024-08-27 16:15:54 +1200

    Subscribed to a shared topic when joining a room

--- src/network/network_behaviour/mdns_behaviour.rs
+++ src/network/network_behaviour/mdns_behaviour.rs
@@ -2,6 +2,8 @@ use libp2p::{Swarm, mdns};
 use crate::logger;
 use crate::network::network::Behaviour;
 use crate::state::APP;
+use libp2p::gossipsub::IdentTopic;
+use std::cmp::Ordering;
 
 
 
@@ -15,7 +17,26 @@ pub async fn handle_event(event: libp2p::mdns::Event, swarm: &mut Swarm<Behaviou
                     let mut app = APP.lock().unwrap();
                     app.peers.push(peer_id.clone());
                     app.peers_no_username.push(peer_id.clone());
-                    app.connected_peers += 1;
+                    app.connected_peers += 1;  
+                    drop(app);
+                    // Create the gossipsub topic by combining `my_peer_id` and `peer_id` alphabetically
+                    // This will make it easy to send DMs to each person
+                    let peer_id_str = peer_id.to_string();
+                    let my_peer_id = swarm.local_peer_id().clone().to_string();
+                    let topic_name = match my_peer_id.cmp(&peer_id_str) {
+                        Ordering::Less => format!("{}_{}", my_peer_id, peer_id_str),
+                        _ => format!("{}_{}", peer_id_str, my_peer_id),
+                    };
+
+                    // Create a gossipsub topic from the combined string
+                    let topic = IdentTopic::new(topic_name.clone());
+                    
+                    // Subscribe to the gossipsub topic
+                    if let Err(e) = swarm.behaviour_mut().gossipsub.subscribe(&topic) {
+                        logger::error!("Failed to subscribe to gossipsub topic {}: {}", topic_name.clone(), e);
+                    } else {
+                        logger::info!("Subscribed to gossipsub topic: {}", topic_name);
+                    }  
             }
         }
 
