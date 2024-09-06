d612251ba4bd58753bb4ccf99d0ec14a214bb1f4
dne33 <dne33@uclive.ac.nz>
2024-09-06 11:16:15 +1000

    Updated dm_screen and select_room

--- src/ui/screens/dm_screen.rs
+++ src/ui/screens/dm_screen.rs
@@ -13,11 +13,11 @@ use std::collections::HashMap;
 use libp2p::{gossipsub, PeerId};
 use std::rc::Rc;
 
+/// Represents the Direct Message (DM) screen state.
 pub struct DmScreen {
-    pub private_messages: HashMap <String, Vec<String>>,
+    pub private_messages: HashMap<String, Vec<String>>,
     pub people_state: ListState,
     pub request_state: ListState,
-
     pub selected_person: usize,
     pub in_sidebar: bool,
     pub in_requests: bool,
@@ -26,6 +26,7 @@ pub struct DmScreen {
 }
 
 impl DmScreen {
+    /// Creates a new `DmScreen` with default state.
     pub fn new() -> Self {
         let mut people_state = ListState::default();
         people_state.select(Some(0));
@@ -43,7 +44,10 @@ impl DmScreen {
         }
     }
 
-    pub fn render(&mut self, frame: &mut Frame,  chunk: Rc<[ratatui::layout::Rect]>, usernames: HashMap<String, String>, peers: Vec<PeerId>) {
+    /// Renders the DM screen including input area, messages, people list, and incoming requests.
+    ///
+    /// Displays the input field, messages with the selected peer, a list of people, and incoming requests.
+    pub fn render(&mut self, frame: &mut Frame, chunk: Rc<[ratatui::layout::Rect]>, usernames: HashMap<String, String>, peers: Vec<PeerId>) {
         let horizontal = Layout::horizontal([
             Constraint::Length(30),
             Constraint::Min(1),
@@ -81,16 +85,14 @@ impl DmScreen {
                 y: input_area.y + 1,
             });
         }
-        
+
         // Retrieve current user's peer ID
-        let current_user_peer_id = {
-            app.my_peer_id.as_ref().map(|id| id.to_string()).unwrap_or_default()
-        };
+        let current_user_peer_id = app.my_peer_id.as_ref().map(|id| id.to_string()).unwrap_or_default();
         
         // Retrieve the selected peer's ID and username
         let selected_peer_id = peers.get(self.selected_person)
-        .map(|peer_id| peer_id.to_string())
-        .unwrap_or_default();
+            .map(|peer_id| peer_id.to_string())
+            .unwrap_or_default();
         let selected_username = usernames.get(&selected_peer_id)
             .cloned()
             .unwrap_or_else(|| "Unknown".to_string());
@@ -100,7 +102,7 @@ impl DmScreen {
         peer_ids.sort(); // Sort alphabetically
         let message_key = peer_ids.join("_");
 
-        // Messages area with the username of the selected peer as the title\
+        // Messages area with the username of the selected peer as the title
         let binding = vec!["Ensure a peer is connected to DM".to_string()];
         let private_messages = app.private_messages.get(&message_key)
             .unwrap_or(&binding)
@@ -118,8 +120,6 @@ impl DmScreen {
             usernames.get(peer).map(|username| ListItem::new(format!("{}", username)))
         })
         .collect();
-        
-
 
         // Sidebar (people list)
         let people_style = if !self.in_sidebar {
@@ -130,7 +130,6 @@ impl DmScreen {
         let people_list = List::new(peer_items)
             .block(Block::default().borders(Borders::ALL).title("People"))
             .style(people_style)
-            // .style()
             .highlight_style(if self.in_sidebar {
                 Style::default().add_modifier(Modifier::BOLD).fg(Color::Yellow)
             } else {
@@ -141,7 +140,16 @@ impl DmScreen {
 
         let current_requests = &app.current_requests;
         self.usernames = usernames.clone();
-        let request_items: Vec<String> = current_requests.iter().map(|request| format!("{} - {}", self.usernames.get(&request.peer_id.to_string()).expect(""), request.request_string)).collect();  
+        let request_items: Vec<String> = current_requests.iter().map(|request| {
+            // Safely retrieve the username using the peer_id
+            match self.usernames.get(&request.peer_id.to_string()) {
+                Some(username) => format!("{} - {}", username, request.request_string),
+                None => {
+                    logger::error!("Username for peer_id {} not found", request.peer_id);
+                    format!("Unknown user - {}", request.request_string)
+                }
+            }
+        }).collect();
         let request_style = if !self.in_requests {
             Style::default().fg(Color::DarkGray)
         } else {
@@ -159,173 +167,200 @@ impl DmScreen {
 
         frame.render_stateful_widget(requests, request_area, &mut self.request_state);
     }
-
+    /// Handles keyboard events for the DM screen.
+    ///
+    /// Processes key inputs for navigating lists, sending messages or requests, and toggling UI modes.
+    /// Returns `Ok(true)` if the Escape key is pressed to exit the application, otherwise `Ok(false)`.
     pub async fn handle_events(&mut self, client: &mut Client, key: KeyEvent) -> Result<bool, std::io::Error> {
         match key.code {
-            KeyCode::Enter => {
-                logger::info!("Enter pressed");
-
-                if self.in_sidebar {
-                    if let Some(peer_id) = self.select_person() {
-                        logger::info!("Selected Peer ID: {:?}", peer_id);
-                        // Use the peer_id as needed here
-                    }
-                    self.in_sidebar = !self.in_sidebar;
-                } else if self.in_requests {
-                    let mut app = APP.lock().unwrap();
-                    logger::info!("Sending File Response");
-                    logger::info!("{:?}", self.request_state.clone());
-
-                    let index = self.request_state.clone().selected().expect("");
-                    let request = app.current_requests.remove(index);
-                    let channel = request.response_channel; // Move the channel out
-                    let input = request.request_string;
-                    
-                    client.send_response(input.clone(), input.clone(), channel).await;
-                    app.input.clear();
-                    app.character_index = 0;
-                } else {
-                    let input = APP.lock().unwrap().input.clone();
-                    
-                    if input.len() != 0 && !input.starts_with("!request file") {
-                        let app = APP.lock().unwrap();
-                        let my_peer_id = match &app.my_peer_id {
-                            Some(peer_id) => peer_id.to_string(),
-                            None => "No Peer ID".to_string(), // Provide a default or placeholder
-                        };
-                        drop(app);
-                        logger::info!("peers: {:?}, selected: {:?}", self.peers.clone(), self.selected_person.clone());
-                        let peer_id = self.peers[self.selected_person].clone().to_string();
-                        // Create a topic by combining and sorting peer IDs alphabetically
-                        let mut peer_ids = vec![my_peer_id.clone(), peer_id.clone()];
-                        peer_ids.sort(); // Sort alphabetically
-
-                        let topic = gossipsub::IdentTopic::new(peer_ids.clone().join("_")); // Join with an appropriate separator
-
-                        // Send the message to the selected peer
-                        client.submit_message(input.clone(), topic.clone()).await;
-
-                        let mut app = APP.lock().unwrap();
-                        app.submit_private_message(peer_ids.join("_"));
-                        // Clear input and reset state
-                        app.input.clear();
-                        app.character_index = 0;
-                    } else if input.starts_with("!request file") {
-                        let mut app = APP.lock().unwrap();
-                        log::info!("Sending File Request");
-                        let input_clone = app.input.clone();
-                        let file: Vec<_> = input_clone.split(" ").collect();
-                        let peer_id = self.peers[self.selected_person].clone();
-                        client.send_request(file.get(file.len()-1).expect("").to_string(), peer_id).await;
-                        app.input.clear();
-                        app.character_index = 0;
-                    } 
-
-                }
-            }
+            KeyCode::Enter => self.handle_enter(client).await,
             KeyCode::Char('~') => {
-                logger::info!("Tilda");
-
-                if self.in_sidebar {
-                    self.in_sidebar = false;
-                    self.in_requests = true;
-                } else if self.in_requests {
-                    self.in_requests = false;
-                } else {
-                    self.in_sidebar = true;
-                }
+                self.toggle_ui_modes();
+                
             }
             KeyCode::Char(to_insert) => {
-                logger::info!("Pressed a Char");
-
-                if !self.in_sidebar {
-                    let mut app = APP.lock().unwrap();
-                    app.enter_char(to_insert);
-                }
+                self.handle_char(to_insert);
             }
             KeyCode::Backspace => {
-                if !self.in_sidebar {
-                    let mut app = APP.lock().unwrap();
-                    app.delete_char();
-                }
+                self.handle_backspace();
             }
             KeyCode::Left => {
-                if !self.in_sidebar {
-                    let mut app = APP.lock().unwrap();
-                    app.move_cursor_left();
-                }
+                self.handle_left();
             }
             KeyCode::Right => {
-                if !self.in_sidebar {
-                    let mut app = APP.lock().unwrap();
-                    app.move_cursor_right();
-                }
+                self.handle_right();
             }
             KeyCode::Up => {
-                if self.in_sidebar {
-                    let user_count = self.usernames.len();
-                    if user_count > 0 {
-                        let i = match self.people_state.selected() {
-                            Some(0) => user_count - 1, // Wrap to the last user
-                            Some(i) => i - 1,
-                            None => 0,
-                        };
-                        self.people_state.select(Some(i));
-                    }
-                } else if self.in_requests {
-                    // Logic for moving up in the request list
-                    let request_count = APP.lock().unwrap().current_requests.len();
-                    if request_count > 0 {
-                        let i = match self.request_state.selected() {
-                            Some(0) => request_count - 1, // Wrap to the last request
-                            Some(i) => i - 1,
-                            None => 0,
-                        };
-                        self.request_state.select(Some(i));
-                    }
-                }
+                self.handle_up();
             }
-
             KeyCode::Down => {
-                if self.in_sidebar {
-                    let user_count = self.usernames.len();
-                    if user_count > 0 {
-                        let i = match self.people_state.selected() {
-                            Some(i) if i >= user_count - 1 => 0, // Wrap to the first user
-                            Some(i) => i + 1,
-                            None => 0,
-                        };
-                        self.people_state.select(Some(i));
-                    }
-                } else if self.in_requests {
-                    let request_count = APP.lock().unwrap().current_requests.len();
-                    // Logic for moving down in the request list
-                    if request_count > 0 {
-                        let i = match self.request_state.selected() {
-                            Some(i) if i >= request_count - 1 => 0, // Wrap to the first request
-                            Some(i) => i + 1,
-                            None => 0,
-                        };
-                        self.request_state.select(Some(i));
-                    }
-                }
+                self.handle_down();
             }
-
-            KeyCode::Esc => {
-                return Ok(true);
+            KeyCode::Esc => return Ok(true),
+            e => {
+                logger::error!("{:?}", e);
             }
-            e => {logger::error!("{:?}", e)}
         }
-        
         Ok(false)
+
     }
 
+    // Handles the enter key press, the resulting action depends which component the user is currently in.
+    async fn handle_enter(&mut self, client: &mut Client) {
+        logger::info!("Enter pressed");
+
+        if self.in_sidebar {
+            if let Some(peer_id) = self.select_person() {
+                logger::info!("Selected Peer ID: {:?}", peer_id);
+            }
+            self.in_sidebar = !self.in_sidebar;
+        } else if self.in_requests {
+            let mut app = APP.lock().unwrap();
+            logger::info!("Sending File Response");
+            logger::info!("{:?}", self.request_state.clone());
+
+            let index = self.request_state.clone().selected()
+                .ok_or_else(|| std::io::Error::new(std::io::ErrorKind::NotFound, "No request selected"));
+            let request = app.current_requests.remove(index.expect("Invalid Index"));
+            let channel = request.response_channel;
+            let input = request.request_string.clone();
+
+            client.send_response(input.clone(), input.clone(), channel).await;
+            app.input.clear();
+            app.character_index = 0;
+        } else {
+            let mut app = APP.lock().unwrap();
+            let input = app.input.clone();
+
+            if !input.is_empty() && !input.starts_with("!request file") {
+                let my_peer_id = app.my_peer_id.as_ref().map_or("No Peer ID".to_string(), |peer_id| peer_id.to_string());
+                logger::info!("peers: {:?}, selected: {:?}", self.peers.clone(), self.selected_person.clone());
+                let peer_id = self.peers[self.selected_person].clone().to_string();
+                let mut peer_ids = vec![my_peer_id, peer_id];
+                peer_ids.sort();
+                let topic = gossipsub::IdentTopic::new(peer_ids.join("_"));
+
+                client.submit_message(input.clone(), topic).await;
+
+                app.submit_private_message(peer_ids.join("_"));
+                app.input.clear();
+                app.character_index = 0;
+            } else if input.starts_with("!request file") {
+                logger::info!("Sending File Request");
+                let file: Vec<_> = input.split_whitespace().collect();
+                let peer_id = self.peers[self.selected_person].clone();
+                client.send_request(file.get(file.len()-1).expect("").to_string(), peer_id).await;
+                app.input.clear();
+                app.character_index = 0;
+            }
+        }
+    }
+
+    /// Toggles between sidebar and request modes.
+    fn toggle_ui_modes(&mut self) {
+        if self.in_sidebar {
+            self.in_sidebar = false;
+            self.in_requests = true;
+        } else if self.in_requests {
+            self.in_requests = false;
+        } else {
+            self.in_sidebar = true;
+        }
+    }
+
+    /// Handles character input, inserting it into the application state if not in sidebar mode.
+    fn handle_char(&mut self, to_insert: char) {
+        logger::info!("Pressed a Char");
+        if !self.in_sidebar {
+            let mut app = APP.lock().unwrap();
+            app.enter_char(to_insert);
+        }
+    }
+
+    /// Handles backspace input, deleting a character from the application state if not in sidebar mode.
+    fn handle_backspace(&mut self) {
+        if !self.in_sidebar {
+            let mut app = APP.lock().unwrap();
+            app.delete_char();
+        }
+    }
+
+    /// Handles left arrow input, moving the cursor left in the application state if not in sidebar mode.
+    fn handle_left(&mut self) {
+        if !self.in_sidebar {
+            let mut app = APP.lock().unwrap();
+            app.move_cursor_left();
+        }
+    }
+
+    /// Handles right arrow input, moving the cursor right in the application state if not in sidebar mode.
+    fn handle_right(&mut self) {
+        if !self.in_sidebar {
+            let mut app = APP.lock().unwrap();
+            app.move_cursor_right();
+        }
+    }
+
+    /// Handles up arrow input, navigating up in the list of users or requests, depending on the current mode.
+    fn handle_up(&mut self) {
+        if self.in_sidebar {
+            let user_count = self.usernames.len();
+            if user_count > 0 {
+                let i = match self.people_state.selected() {
+                    Some(0) => user_count - 1,
+                    Some(i) => i - 1,
+                    None => 0,
+                };
+                self.people_state.select(Some(i));
+            }
+        } else if self.in_requests {
+            let request_count = APP.lock().unwrap().current_requests.len();
+            if request_count > 0 {
+                let i = match self.request_state.selected() {
+                    Some(0) => request_count - 1,
+                    Some(i) => i - 1,
+                    None => 0,
+                };
+                self.request_state.select(Some(i));
+            }
+        }
+    }
+
+    /// Handles down arrow input, navigating down in the list of users or requests, depending on the current mode.
+    fn handle_down(&mut self) {
+        if self.in_sidebar {
+            let user_count = self.usernames.len();
+            if user_count > 0 {
+                let i = match self.people_state.selected() {
+                    Some(i) if i >= user_count - 1 => 0,
+                    Some(i) => i + 1,
+                    None => 0,
+                };
+                self.people_state.select(Some(i));
+            }
+        } else if self.in_requests {
+            let request_count = APP.lock().unwrap().current_requests.len();
+            if request_count > 0 {
+                let i = match self.request_state.selected() {
+                    Some(i) if i >= request_count - 1 => 0,
+                    Some(i) => i + 1,
+                    None => 0,
+                };
+                self.request_state.select(Some(i));
+            }
+        }
+    }
+
+
+    /// Selects a person from the list based on the current selection state.
+    ///
+    /// Updates the `selected_person` index if a valid selection is made.
     fn select_person(&mut self) -> Option<String> {
         if let Some(selected) = self.people_state.selected() {
             if selected < self.peers.len() {
                 let peer_id = self.peers[selected].to_string();
                 logger::info!("Selected peer ID: {:?}", peer_id);
-                self.selected_person = selected.clone();
+                self.selected_person = selected;
             }
         }
         None
--- src/ui/screens/select_room_screen.rs
+++ src/ui/screens/select_room_screen.rs
@@ -7,83 +7,67 @@ use ratatui::{
 use std::rc::Rc;
 use crate::state::Screen::MainScreen;
 
-// Function to render the room list
-pub fn render(frame: &mut Frame,  chunk: Rc<[ratatui::layout::Rect]>) {
+/// Renders the list of chat rooms and the current room display.
+///
+/// Displays a list of available rooms and highlights the currently selected room.
+/// Also shows the title of the current room in a bordered block.
+pub fn render(frame: &mut Frame, chunk: Rc<[ratatui::layout::Rect]>) {
     let mut app = APP.lock().unwrap();
 
-    // Define the layout for the rooms
     let rooms_area = Layout::default()
         .direction(Direction::Vertical)
         .constraints([Constraint::Min(1)])
         .split(chunk[1]);
 
-    // Create the list of room items
     let room_items: Vec<ListItem> = app
         .rooms
         .iter()
         .map(|room| ListItem::new(room.clone()))
         .collect();
 
-    // Create the List widget
     let rooms_list = List::new(room_items)
         .block(Block::default().borders(Borders::ALL).title("Rooms"))
         .highlight_style(Style::default().add_modifier(Modifier::REVERSED))
         .highlight_symbol(">>");
 
-    // Render the list widget with state
     frame.render_stateful_widget(rooms_list, rooms_area[0], &mut app.room_state);
 
-   
     let current_room = &app.rooms[app.current_room];
     let current_room_display = Block::default()
         .title(format!("Current Room: {}", current_room))
         .borders(Borders::ALL);
     frame.render_widget(current_room_display, rooms_area[0]);
-    
 }
 
-// Function to handle key events for room navigation
+/// Handles keyboard events for room navigation and selection.
+///
+/// Processes key inputs to navigate through the list of rooms and select a room.
+/// Updates the application state accordingly. Returns `Ok(true)` if the Escape
+/// key is pressed to exit the application, otherwise `Ok(false)`.
 pub async fn handle_events(key: KeyEvent) -> Result<bool, std::io::Error> {
     let mut app = APP.lock().unwrap();
     match key.code {
         KeyCode::Enter => {
-            let _i = match app.room_state.selected() {
-                Some(i) => {
-                    app.current_room = i;
-                    app.current_screen = MainScreen;
-                }
-                None => {},
-            };
+            if let Some(i) = app.room_state.selected() {
+                app.current_room = i;
+                app.current_screen = MainScreen;
+            }
         }
         KeyCode::Up => {
             let i = match app.room_state.selected() {
-                Some(i) => {
-                    if i == 0 {
-                        app.rooms.len() - 1
-                    } else {
-                        i - 1
-                    }
-                }
+                Some(i) => if i == 0 { app.rooms.len() - 1 } else { i - 1 },
                 None => 0,
             };
             app.room_state.select(Some(i));
         }
         KeyCode::Down => {
             let i = match app.room_state.selected() {
-                Some(i) => {
-                    if i >= app.rooms.len() - 1 {
-                        0
-                    } else {
-                        i + 1
-                    }
-                }
+                Some(i) => if i >= app.rooms.len() - 1 { 0 } else { i + 1 },
                 None => 0,
             };
             app.room_state.select(Some(i));
         }
-        KeyCode::Esc => {
-            return Ok(true); // Exit the application
-        }
+        KeyCode::Esc => return Ok(true),
         _ => {}
     }
     Ok(false)
